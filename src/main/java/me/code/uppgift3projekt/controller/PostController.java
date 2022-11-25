46e3cc9b2a9b160cde764aae8ef3563705f4605a
AnPe92 <93088524+AnPe92@users.noreply.github.com>
2022-11-25 09:43:10 +0100

    Changes by oklim

--- src/main/java/me/code/uppgift3projekt/controller/PostController.java
+++ src/main/java/me/code/uppgift3projekt/controller/PostController.java
@@ -1,7 +1,15 @@
 package me.code.uppgift3projekt.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @RestController
 public class PostController {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("title", "Blogg");
+        return "home";
+    }
 }
